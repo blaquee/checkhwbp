9f5fa2d127ad238590d83826e67c708c24c1bf6d
genuine <blaquee@gmail.com>
2017-03-07 15:46:48 -0500

    +1

--- CheckkHwBP/Source.cpp
+++ CheckkHwBP/Source.cpp
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 
 
-#pragma comment(linker,"/include:__tls_used") // This will cause the linker to create the TLS directory
-#pragma section(".CRT$XLB",read) // Create a new section
-
 DWORD CALLBACK CheckBP(LPVOID args)
 {
 	HANDLE thread = (HANDLE)args;
@@ -50,11 +47,10 @@ void CALLBACK handle_hwbp(PVOID hHandle, DWORD dwReason, PVOID)
 		);
 		printf("Starting thread..\n");
 		hThread = CreateThread(NULL, 0, CheckBP, (LPVOID)hDupThread, 0, &hThreadID);
-		//WaitForSingleObject(hThread, INFINITE);
 		//CloseHandle(hThread);
 	}
 }
-__declspec(allocate(".CRT$XLB")) PIMAGE_TLS_CALLBACK CallbackAddress[] = { handle_hwbp, NULL };
+
 /*
 #ifdef _WIN64
 #pragma comment (linker, "/INCLUDE:_tls_used")  // See p. 1 below
@@ -80,6 +76,45 @@ PIMAGE_TLS_CALLBACK tls_callback_func = handle_hwbp;
 #endif //_WIN64
 */
 
+#if defined(_MSC_VER)
+
+	#ifdef _WIN64
+	#pragma comment (linker, "/INCLUDE:_tls_used")  // See p. 1 below
+	#pragma comment (linker, "/INCLUDE:tls_callback_func")  // See p. 3 below
+	#pragma const_seg(".CRT$XLB")
+	EXTERN_C const
+	#else
+	#pragma comment (linker, "/INCLUDE:__tls_used")  // See p. 1 below
+	#pragma comment (linker, "/INCLUDE:_tls_callback_func")  // See p. 3 below
+	#pragma data_seg(".CRT$XLB")
+	EXTERN_C
+	#endif
+	PIMAGE_TLS_CALLBACK tls_callback_func = handle_hwbp;
+	#ifdef _WIN64
+	#pragma const_seg()
+	#else
+	#pragma data_seg()
+	#endif
+
+#elif defined(__GNUC__)
+	PIMAGE_TLS_CALLBACK tls_callback_func __attribute__((section(".CRT$XLB"))) = handle_hwbp;
+#endif
+
+/*
+#if defined(_MSC_VER)
+#	pragma comment (linker, "/INCLUDE:_tls_callback_func")
+#   if defined (_M_IX86)
+#	   pragma comment(linker, "/INCLUDE:__tls_used")
+#   else
+#	   pragma comment(linker, "/INCLUDE:_tls_used")
+#   endif
+#   pragma data_seg(".CRT$XLB")
+PIMAGE_TLS_CALLBACK tls_callback_func = handle_hwbp;
+#   pragma data_seg()
+#elif defined(__GNUC__)
+PIMAGE_TLS_CALLBACK tls_callback_func __attribute__((section(".CRT$XLB"))) = handle_hwbp;
+#endif
+*/
 int main(int argc, char** argv)
 {
 	printf("waiting..");
